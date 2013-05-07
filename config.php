6e77b077708b472fc4d1a429efb947932274c314
Robert de Jong <robertde.jong@live.nl>
2013-05-07 14:20:41 +0200

    Config file toegevoegd

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+//Database
+define("DATABASE_USERNAME", "p4p");
+define("DATABASE_PASSWORD", "geheim");
+define("DATABASE_HOST", "localhost");
+define("DATABASE_PORT", "3306");
+define("DATABASE_NAME", "p4p");
+
+//Debug
+define("DEBUG", true);
+?>
--- data/validator.php
+++ data/validator.php
@@ -1,5 +1,18 @@
 <?php
+
+/**
+ * @Auteur Robert de Jong
+ * @Datum 13-5-2013
+ */
+ 
 class validator{
     
+    static function isInt($getal){
+        return is_numeric($getal);
+    }
+    
+    static function isString($string){
+        return is_string($string);
+    }
 }
 ?>
