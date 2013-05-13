c69c8d0253d5dd9acaad1f118dcc3209f63977cd
Wim Dalof <wdalof@hotmail.com>
2013-05-13 13:45:31 +0200

    Not working class

--- admin/trajecten.php
+++ admin/trajecten.php
@@ -1,15 +1,20 @@
 <?php
 
 /**
- * @author 
+ * @Wim Dalof
  * @copyright 2013
  */
+
+require("../data/DbHandler.php");
 
 class Trajecten
 {
     public $test = 0;
     
-    
+    function addTraject($startId, $stopId)
+    {
+        
+    }
 }
 
 ?>
