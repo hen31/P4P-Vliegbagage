ad786b06ff090ffded12e5f185e80948c1797ca5
Wim Dalof <wdalof@hotmail.com>
2013-05-13 14:30:38 +0200

    Moved the file to another folder

---
+++ data/trajecten.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @author 
+ * @copyright 2013
+ */
+
+require("includeAll.php");
+class trajecten
+{
+    public function addTraject($startId, $stopId)
+    {
+        echo("hallo");
+    }
+}
+
+?>
