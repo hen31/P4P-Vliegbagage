ba7ee34702f7ab8b2882aec213784ba9095f5b17
Robert de Jong <robertde.jong@live.nl>
2013-05-13 10:09:16 +0200

    Validator Class

--- data/validator.php
+++ data/validator.php
@@ -8,11 +8,40 @@
 class validator{
     
     static function isInt($getal){
-        return is_numeric($getal);
+        return is_int($getal);
     }
     
     static function isString($string){
         return is_string($string);
+    }
+    
+    static function isEmail($email){
+        $regex = "/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/";
+        return preg_match($regex, $email);
+    }
+    
+    static function stringLimit($min, $max, $string){
+        $strlen = strlen($string);
+        if($strlen < $min){
+            return false;
+        }
+        if($strlen > $max){
+            return false;
+        }
+        return true;
+    }
+    
+    static function isValuta($bedrag){
+        if(!is_float($bedrag)){
+            return false;
+        }
+        
+        $round = round($bedrag, 2);
+        if($round !== $bedrag){
+            return false;
+        }
+        
+        return true;
     }
 }
 ?>
