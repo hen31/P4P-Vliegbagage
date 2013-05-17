786cb5b88bbf3d6455a187897da905a92042029f
Matthe Jacobs <m_jacobs@hotmail.nl>
2013-05-17 09:57:34 +0200

    user1.0

---
+++ admin/users.php
@@ -0,0 +1,19 @@
+<?php
+require_once("includeAll.php");
+class user
+{
+public $id;
+public $userName;
+    public static function createUser($username,$userPassword)
+    {
+        $userId;
+                               
+    }
+    
+    public static function login()
+    {
+                
+    }   
+}
+
+?>
