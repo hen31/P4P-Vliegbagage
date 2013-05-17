d725e117abed076a7334acd2e87af8b03194d99d
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2013-05-17 09:31:27 +0200

    User onderkant

--- data/users.php
+++ data/users.php
@@ -41,22 +41,69 @@ class user
         $userId;
                                
     }
-    
+    //Gebruiker ophalen aan de hand van de id die wordt opgegeven
     public static function GetUser($userid)
     {
-        $userId;
+        $results = DbHandler::Query("SELECT * FROM user WHERE user_id=:id;", array("id"=>$userid));
+        if($results !=null)
+        {
+            $gebruiker = new user();
+            
+          $gebruiker->id  = $results[0]["user_id"];
+           $gebruiker->userName  = $results[0]["username"];
+           return $gebruiker;
+        }
+        else
+        {
+            return null;
+        }
                                
     }
-    
+    //Alle gebruikers die in de database staan terug geven afhankelijk van de zoekterm en de hoeveelste gebruikers
     public static function GetUsers($searchTerm, $start,$end)
     {
-        $userId;
-                               
+            $results = DbHandler::Query("SELECT * FROM user WHERE username LIKE :search LIMIT :beg, :end;", array(
+            "search"=> '%'.$searchTerm.'%', "beg"=>$start,"end"=>$end));
+            //als gebruikers leeg is null terug geven
+        if($results !=null)
+        {
+      $gebruikers = array();
+            for($i =0;$i<count($results);$i++)
+            {
+            $gebruiker = new user();
+            $gebruiker->id  = $results[$i]["user_id"];
+            $gebruiker->userName  = $results[$i]["username"];
+            //Gebruiker toevoegen aan array
+            $gebruikers[] = $gebruiker;
+           }
+           return $gebruikers;
+        }
+        else
+        {
+            return null;
+        }                       
     }
-    
+    //fucntie om te checken of het een geldige login is. 
+    //als dit het geval is dan wordt er een object van gebruiker terug gegeven.
     public static function login($username,$userPassword)
     {
-     
-    }   
+      $results = DbHandler::Query("SELECT user_id, username FROM user WHERE username=:user AND password=:pass;", array(
+            "user"=> $username, "pass"=>$userPassword));
+            //als er geen gebruiker bestaat met deze combinatie wordt null terug gegeven
+        if($results !=null)
+        {
+            
+            $gebruiker = new user();
+            
+          $gebruiker->id  = $results[0]["user_id"];
+           $gebruiker->userName  = $results[0]["username"];
+           return $gebruiker;
+        }
+        else
+        {
+         //null terug geven   
+            return null;
+        }
+    }
 }
 ?>
