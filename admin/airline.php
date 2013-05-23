4a8e47a3ecd0e09ef62c75001e21e5855cccbeb0
Robert de Jong <robertde.jong@live.nl>
2013-05-23 14:38:59 +0200

    Style update en alle velden toegevoegd om een airline toe te voegen

--- admin/airline.php
+++ admin/airline.php
@@ -49,7 +49,49 @@ if(isset($_GET["action"]) && $_GET["action"] == "add"){
     <h1 style="margin-right: 20px;">Class toevoegen aan vliegmaatschappij</h1><br />
     
     <form action="airline.php" method="post" class="form">
-        <label>Test</label><input type="text" name="" />
+        <label>Vliegmaatschappij:</label><input type="text" name="airline" />
+        <label>Class:</label><select class="input" name="classnumber">
+                                <option></option>
+                                <option value="0">Economy</option>
+                                <option value="1">Eerste klas</option>
+                                <option value="2">Business klas</option>
+                            </select>
+        <label>Stukken handbagage:</label><input type="text" name="pcsHL" />
+        <label>Max. gewicht handbagage:</label><input type="text" name="MaxWeightHL" />
+        <label>Lengte handbagage:</label><input type="text" name="sizeLenghtHL" />
+        <label>Hoogte handbagage:</label><input type="text" name="sizeHeightHL" />
+        <label>Breedte handbagage:</label><input type="text" name="SizeWidthHL" />
+        <label>Grootte handbagage:</label><input type="text" name="sizeTotalHL" />
+        <label>Laptop toegestaan:</label><select class="input" name="LaptopAllowedHL"><option></option><option value="true">Ja</option><option value="false">Nee</option></select>
+        <label>Kinderwagen toegestaan:</label><select class="input" name="strollerAllowedHL"><option></option><option value="true">Ja</option><option value="false">Nee</option></select>
+        <label>Stukken handbagage kind:</label><input type="text" name="pcsInfantHL" />
+        <label>Stukken bagage kind:</label><input type="text" name="pcsLuggageInfant" />
+        <label>Max. gewcht bagage kind:</label><input type="text" name="pcsLuggageInfantMaxWeight" />
+        <label>Stukken bagage:</label><input type="text" name="pcsLuggage" />
+        <label>Max. gewicht bagage:</label><input type="text" name="maxWeightLuggage" />
+        <label>Loyalty programma (LP):</label><select class="input" name="LoyaltyProgramme"><option></option><option value="true">Ja</option><option value="false">Nee</option></select>
+        <label>Extra stukken bagage LP:</label><input type="text" name="LPextraPcsLuggage" />
+        <label>Extra gewicht bagage LP:</label><input type="text" name="LPextraWeightLuggage" />
+        <label>Abs. max. gewicht bagage:</label><input type="text" name="AbsoluteMaxPerItem" />
+        <br />
+        <label>Lengte per item:</label><input type="text" name="sizeLenghtPerItem" />
+        <label>Hoogte per item:</label><input type="text" name="sizeHeightPerItem" />
+        <label>Breedte per item:</label><input type="text" name="sizeWidthPerItem" />
+        <label>Grootte per item:</label><input type="text" name="sizeTotalPerItem" />
+        <br />
+        <label>Pooling:</label><select name="Pooling" class="input"><option></option><option value="true">Ja</option><option value="false">Nee</option></select>
+        <label>Gratis rolstoel:</label><select name="FreeWheelChair" class="input"><option></option><option value="true">Ja</option><option value="false">Nee</option></select>
+        <label>Gratis blindengeleidehond:</label><select name="FreeServiceDog" class="input"><option></option><option value="true">Ja</option><option value="false">Nee</option></select>
+        <label>Huisdieren toegestaan:</label><select name="PetsAllowed" class="input"><option></option><option value="true">Ja</option><option value="false">Nee</option></select>
+        <label>Max. gewicht huisdier:</label><input type="text" name="MaxWeightPet" />
+        <label>Lengte huisdier:</label><input type="text" name="sizeLenghtPet" />
+        <label>Hoogte huisdier:</label><input type="text" name="sizeHeightPet" />
+        <label>Breedte huisdier:</label><input type="text" name="sizeWidthPet" />
+        <label>Grootte huisdier:</label><input type="text" name="sizeTotalPet" />
+        <label>Declaration of value</label><input type="text" name="DeclarationOfValue" />
+        <label>Max. declartion of value</label><input type="text" name="MaxDeclarationOfValue" />
+    
+        <label>&nbsp;</label><input type="submit" value="Opslaan" />
     </form>
 </div>
 <div style="clear: both;"></div>
