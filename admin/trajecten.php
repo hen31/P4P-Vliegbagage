299cffc6cb2884e8f13e9c51aeb22d55f2093289
Wim Dalof <wdalof@hotmail.com>
2013-05-27 08:40:49 +0200

    Added filter, pagination is correctly implemented now

--- admin/trajecten.php
+++ admin/trajecten.php
@@ -3,70 +3,169 @@
 require_once ("../data/includeAll.php");
 $titel = "Trajecten";
 require_once ("bovenkant.php");
+print_r($_POST);
 ?>
 <!-- Hier alles neerzetten-->
 
+<script type="text/javascript">
+	function expand(a) {
+		var e = document.getElementById(a);
+		if (!e) return true;
+		if (e.style.display == "none") {
+			e.style.display = "block"
+		} else {
+			e.style.display = "none"
+		}
+		return true;
+	}
+</script>
+
 <h1>Traject toevoegen</h1>
+<p>Via onderstaand formulier kunt u een nieuw traject toevoegen aan de database. Selecteer een begin- en eindpunt, en klik vervolgens op "Toevoegen".</p>
 <form action="trajecten.php" method="post">
   <div class="ui-widget">
     <label for="beginPunt">Beginpunt: </label>
     <input name="beginPunt" id="beginPunt" />
     <label for="eindPunt">Eindpunt: </label>
-    <input name="eindPunt"  id="eindPunt"  />
+    <input name="eindPunt" id="eindPunt"  />
     <input id="submit" type="submit" value="Toevoegen" />
-    <input type="hidden" name="checkPosted" value="yes" />
+    <input type="hidden" name="checkPostedAdd" value="yes" />
   </div>
 </form>
 <br />
+
 <h1>Aanwezige trajecten</h1>
+<p>In onderstaande tabel ziet u een overzicht van de trajecten die momenteel aanwezig zijn in de database. Indien gewenst kunt u filter instellen om specifieke trajecten weer te geven.</p>
+<input type="button" onclick="return expand('Filter')" value="Filter instellingen"/>
 <br />
+<div id="Filter" style="display:none">
+<br />
+<form action="trajecten.php" method="post" >
+  <div class="ui-widget">
+    <label for="filterBeginPunt">Beginpunt: </label>
+    <input name="filterBeginPunt" id="beginPunt" />
+    <label for="filterEindPunt">Eindpunt: </label>
+    <input name="filterEindPunt"  id="eindPunt"  />
+    <input type="hidden" name="checkPostedFilter" value="yes" />
+    <input name="action" type="submit" value="Filter" />
+    <input name="action" type="submit" value="Verwijder filter" />
+    </div>
+</form>
+</div>
+<br />
+
 <table width="100%" border="0">
 <?php
 
-#trajecten::GetAllTrajecten();
+if (isset($_POST["checkPostedFilter"])) {
+    if ($_POST["action"] == "Verwijder filter") {
+        $_SESSION["filterBeginPunt"] = 0;
+        $_SESSION["filterEindPunt"] = 0;
+        $_SESSION["filterId"] = 0;
+    }
+    if ($_POST["action"] == "Filter") {
+        if (empty($_POST["filterBeginPunt"]) && empty($_POST["filterEindPunt"])) {
+            $_SESSION["filterId"] = 0;
+            echo '<script type="text/javascript"> window.alert("Beginpunt en eindpunt mogen niet beide leeg zijn.")</script>';
+        }
+        if ($_POST["filterBeginPunt"] && $_POST["filterEindPunt"]) {
 
-if (!empty($_SERVER['QUERY_STRING'])) {
-	if (isset($_GET['remove'])) {
-		$remove = $_GET['remove'];
+            $startAirportName = airports::GetAirportByName($_POST["filterBeginPunt"]);
+            $startAirportId = $startAirportName->AirportID;
 
-		trajecten::RemoveItem($remove);
-	}
+            $stopAirportName = airports::GetAirportByName($_POST["filterEindPunt"]);
+            $stopAirportId = $stopAirportName->AirportID;
+
+            $_SESSION["filterBeginPunt"] = $startAirportId;
+            $_SESSION["filterEindPunt"] = $stopAirportId;
+            $_SESSION["filterId"] = 1;
+        }
+        if ($_POST["filterBeginPunt"] && empty($_POST["filterEindPunt"])) {
+
+            $startAirportName = airports::GetAirportByName($_POST["filterBeginPunt"]);
+            $startAirportId = $startAirportName->AirportID;
+
+            $_SESSION["filterBeginPunt"] = $startAirportId;
+
+            $_SESSION["filterEindPunt"] = 0;
+            $_SESSION["filterId"] = 2;
+        }
+        if (empty($_POST["filterBeginPunt"]) && $_POST["filterEindPunt"]) {
+
+            $stopAirportName = airports::GetAirportByName($_POST["filterEindPunt"]);
+            $stopAirportId = $stopAirportName->AirportID;
+
+            $_SESSION["filterBeginPunt"] = 0;
+            $_SESSION["filterEindPunt"] = $stopAirportId;
+            $_SESSION["filterId"] = 3;
+        }
+    }
+} else {
+    if (!isset($_SESSION["filterId"])) {
+        $_SESSION["filterBeginPunt"] = 0;
+        $_SESSION["filterEindPunt"] = 0;
+        $_SESSION["filterId"] = 0;
+    }
 }
 
 if (!empty($_SERVER['QUERY_STRING'])) {
-	if (isset($_GET['resultid'])) {
-		$id = $_GET['resultid'];
-		$begin = $_GET['resultid'] * 5;
-		$end = ($begin + 5);	
-	}
+    if (isset($_GET['remove'])) {
+        $remove = $_GET['remove'];
+
+        trajecten::RemoveItem($remove);
+        $id = 0;
+        $begin = $id;
+        $end = ($begin + 5);
+    }
 }
-else{
-	$id = 0;
-	$begin = $id;
-	$end = ($begin + 5);
+if (!empty($_SERVER['QUERY_STRING'])) {
+    if (isset($_GET['add'])) {
+        echo ("fck");
+    }
 }
 
-$idmin = $id -1;
-$idplus = $id +1;
+if (!empty($_SERVER['QUERY_STRING'])) {
+    if (isset($_GET['resultid'])) {
+        $id = $_GET['resultid'];
+        $begin = $_GET['resultid'] * 5;
+        $end = ($begin + 5);
+    }
 
-/**
- * if($idmin < 0){
- *     
- *     $idmin = 0;
- * }
- */
+} else {
+    $id = 0;
+    $begin = $id;
+    $end = ($begin + 5);
+}
+$idmin = $id - 1;
+$idplus = $id + 1;
 
-$result = trajecten::GetAllTrajecten($begin);
+#print_r($_SESSION);
+
+if (isset($_SESSION["filterId"])) {
+    if ($_SESSION["filterId"] == 0) {
+        $result = trajecten::GetAllTrajecten($begin, null, null);
+    }
+    if ($_SESSION["filterId"] == 1) {
+        $result = trajecten::GetAllTrajecten($begin, $_SESSION["filterBeginPunt"], $_SESSION["filterEindPunt"]);
+    }
+    if ($_SESSION["filterId"] == 2) {
+        $result = trajecten::GetAllTrajecten($begin, $_SESSION["filterBeginPunt"], null);
+    }
+    if ($_SESSION["filterId"] == 3) {
+        $result = trajecten::GetAllTrajecten($begin, null, $_SESSION["filterEindPunt"]);
+    }
+} else {
+    $result = trajecten::GetAllTrajecten($begin, null, null);
+}
 $count = count($result);
 
 if (count($result) != 0) {
 ?>
-	<td>Beginpunt</td>
-	<td>Eindpunt</td>
-	<td>Actie</td>
+	<td><b>Beginpunt</b></td>
+	<td><b>Eindpunt</b></td>
+	<td><b>Actie</b></td>
 <?php
-for ($i = 0; $i < $count; $i++)
-{
+    for ($i = 0; $i < $count; $i++) {
 ?>
 	<tr>
 <td><?php echo ($result[$i]['airport_start_id']->AirportName); ?></td>
@@ -74,22 +173,26 @@ for ($i = 0; $i < $count; $i++)
 <td><a href="trajecten.php?remove=<?php echo $result[$i]['traject_id'] ?>">Verwijder</a></td>
 </tr>
 <?php
-}
+    }
 } else {
-	echo ("Er zijn geen trajecten aanwezig.");
+    echo ("Er zijn geen trajecten aanwezig.");
 }
 ?>
 </table>
 <?php
-if($idmin > 0 || $idmin == 0)
-{
+if ($idmin > 0 || $idmin == 0) {
 ?>
 	<a href="trajecten.php?resultid=<?php echo $idmin ?>">Vorige</a>
 	<?php
 }
-?>
 
-<a href="trajecten.php?resultid=<?php echo $idplus ?>">Volgende</a>
+if (($idplus * 5) < trajecten::GetTrajectAmount($_SESSION["filterId"], $_SESSION["filterBeginPunt"],
+    $_SESSION["filterEindPunt"])) {
+?>
+	<a href="trajecten.php?resultid=<?php echo $idplus ?>">Volgende</a>
+    <?php
+}
+?>
 
 <script src="../js/jquery-1.9.0.min.js"></script>
 <script src="../js/jquery-ui.js"></script>
@@ -100,15 +203,15 @@ if($idmin > 0 || $idmin == 0)
   $(function() {
     var availableTags = [
     <?php
-    $airports = airports::GetAirports();
-    for ($i = 0; $i < count($airports); $i++) {
-    	if ($i == count($airports) - 1) {
+$airports = airports::GetAirports();
+for ($i = 0; $i < count($airports); $i++) {
+    if ($i == count($airports) - 1) {
 
-    		echo '"' . $airports[$i]->AirportName . '"';
-    	} else {
-    		echo '"' . $airports[$i]->AirportName . '"' . ",";
-    	}
-    } ?>
+        echo '"' . $airports[$i]->AirportName . '"';
+    } else {
+        echo '"' . $airports[$i]->AirportName . '"' . ",";
+    }
+} ?>
     ];
     $( "#beginPunt" ).autocomplete({
       source: availableTags
@@ -118,26 +221,65 @@ if($idmin > 0 || $idmin == 0)
     });
   });
   </script>
+  
 <?php
 require_once ("onderkant.php");
 ?>
 <?php
-if ((isset($_POST["checkPosted"]))) {
-	if (($_POST["beginPunt"] == $_POST["eindPunt"])) {
-		echo '<script type="text/javascript"> window.alert("Beginpunt en eindpunt mogen niet gelijk zijn.")</script>';
-	}
-	if (!($_POST["beginPunt"] == $_POST["eindPunt"])) {
-		if ((!$_POST["beginPunt"] || !$_POST["eindPunt"])) {
-			echo '<script type="text/javascript"> window.alert("Beginpunt en eindpunt mogen niet leeg zijn.")</script>';
-		} else {
-			try {
-				trajecten::AddItem($_POST["beginPunt"], $_POST["eindPunt"]);
-				echo '<script type="text/javascript"> window.alert("Traject is met success toegevoegd.")</script>';
-			}
-			catch (exception $e) {
-				echo '<script type="text/javascript"> window.alert("Er ging iets mis met het toevoegen van het traject. Probeer het opnieuw altublieft.")</script>';
-			}
-		}
-	}
+if ((isset($_POST["checkPostedAdd"]))) {
+    if (($_POST["beginPunt"] == $_POST["eindPunt"])) {
+        echo '<script type="text/javascript"> window.alert("Beginpunt en eindpunt mogen niet gelijk zijn.")</script>';
+    }
+    if (!($_POST["beginPunt"] == $_POST["eindPunt"])) {
+        if ((!$_POST["beginPunt"] || !$_POST["eindPunt"])) {
+            echo '<script type="text/javascript"> window.alert("Beginpunt en eindpunt mogen niet leeg zijn.")</script>';
+        } else {
+            if (!airports::GetAirportByName($_POST["beginPunt"]) || !airports::
+                GetAirportByName($_POST["eindPunt"])) {
+                echo '<script type="text/javascript"> window.alert("Het door u ingevoerde beginpunt of eindpunt bestaat niet. Probeer het opnieuw alstublieft.")</script>';
+            } else {
+                if (trajecten::CheckExist($_POST["beginPunt"], $_POST["eindPunt"])) {
+                    echo '<script type="text/javascript"> window.alert("Het door u ingevoerde traject bestaat al. Probeer het opnieuw alstublieft.")</script>';
+                } else {
+                    try {
+                        trajecten::AddItem($_POST["beginPunt"], $_POST["eindPunt"]);
+                        $_POST = array();                        
+                        #                        echo '<script type="text/javascript"> window.alert("Traject is met success toegevoegd."); window.location.refresh();</script>';
+                        #echo '<script type="text/javascript"> window.alert("Traject is met success toegevoegd."); window.location = "trajecten.php;</script>';
+                    }
+                    catch (exception $e) {
+                        echo '<script type="text/javascript"> window.alert("Er ging iets mis met het toevoegen van het traject. Probeer het opnieuw altublieft.")</script>';
+                    }
+                }
+            }
+        }
+    }
 }
+
+#if ((isset($_POST["checkPostedAdd"]))) {
+#    if (($_POST["beginPunt"] == $_POST["eindPunt"])) {
+#        echo '<script type="text/javascript"> window.alert("Beginpunt en eindpunt mogen niet gelijk zijn.")</script>';
+#    }
+#    if (!($_POST["beginPunt"] == $_POST["eindPunt"])) {
+#        if ((!$_POST["beginPunt"] || !$_POST["eindPunt"])) {
+#            echo '<script type="text/javascript"> window.alert("Beginpunt en eindpunt mogen niet leeg zijn.")</script>';
+#        } else {
+#            if (!airports::GetAirportByName($_POST["beginPunt"]) || !airports::
+#                GetAirportByName($_POST["eindPunt"])) {
+#                echo '<script type="text/javascript"> window.alert("Het door u ingevoerde beginpunt of eindpunt bestaat niet. Probeer het opnieuw alstublieft.")</script>';
+#            } else {
+#                try {
+#                    trajecten::AddItem($_POST["beginPunt"], $_POST["eindPunt"]);
+#                    #                        echo '<script type="text/javascript"> window.alert("Traject is met success toegevoegd."); window.location.refresh();</script>';
+#                    #echo '<script type="text/javascript"> window.alert("Traject is met success toegevoegd."); window.location = "trajecten.php;</script>';
+#                }
+#                catch (exception $e) {
+#                    echo '<script type="text/javascript"> window.alert("Er ging iets mis met het toevoegen van het traject. Probeer het opnieuw altublieft.")</script>';
+#                }
+#            }
+#
+#        }
+#    }
+#}
+
 ?>
--- data/trajecten.php
+++ data/trajecten.php
@@ -13,7 +13,7 @@ class trajecten
     public $Airport2;
 
     //Add a new traject.
-    public function AddItem($startAirport, $stopAirport)
+    public static function AddItem($startAirport, $stopAirport)
     {
         $start = airports::GetAirportByName($startAirport);
         $startAirportId = $start->AirportID;
@@ -26,60 +26,90 @@ class trajecten
     }
 
     //Remove an existing traject.
-    public function RemoveItem($trajectId)
+    public static function RemoveItem($trajectId)
     {
         DbHandler::NonQuery("DELETE FROM traject WHERE traject_id = :trajectId;", array
             ("trajectId" => $trajectId));
     }
-    
-    public function GetTrajectAmount()
+
+    public static function GetTrajectAmount($filterId, $filterBeginPunt, $filterEindPunt)
     {
-		$result = DbHandler::QueryScalar("SELECT COUNT(*) FROM traject;",null);
-        
+        if ($filterId == 0) {
+            $result = DbHandler::QueryScalar("SELECT COUNT(*) FROM traject;", null);
+        }
+        if ($filterId == 1) {
+            $result = DbHandler::QueryScalar("SELECT COUNT(*) FROM traject WHERE airport_start_id = :filterBeginPunt && airport_stop_id = :filterEindPunt;",
+                array("filterBeginPunt" => $filterBeginPunt, "filterEindPunt" => $filterEindPunt));
+        }
+        if ($filterId == 2) {
+            $result = DbHandler::QueryScalar("SELECT COUNT(*) FROM traject WHERE airport_start_id = :filterBeginPunt;",
+                array("filterBeginPunt" => $filterBeginPunt));
+        }
+        if ($filterId == 3) {
+            $result = DbHandler::QueryScalar("SELECT COUNT(*) FROM traject WHERE airport_stop_id = :filterEindPunt;",
+                array("filterEindPunt" => $filterEindPunt));
+        }
+
         return $result;
     }
-    
-     public function GetAllTrajecten($begin)
-     {
-#         $result = DbHandler::Query("SELECT * FROM traject LIMIT :begin, :end;", array("begin" => $begin, "end" => $end));
-          $result = DbHandler::Query("SELECT * FROM traject LIMIT " . $begin . ", 5", null);
-         
-         for($i = 0; $i < count($result); $i++)
-         {
+
+    public static function CheckExist($startAirport, $stopAirport)
+    {     
+        $start = airports::GetAirportByName($startAirport);
+        $startAirportId = $start->AirportID;
+
+        $stop = airports::GetAirportByName($stopAirport);
+        $stopAirportId = $stop->AirportID;
+
+        $result = DbHandler::QueryScalar("SELECT * FROM traject WHERE airport_start_id = :startAirportId AND airport_stop_id = :stopAirportId;",
+            array("startAirportId" => $startAirportId, "stopAirportId" => $stopAirportId));
+        if (count($result) == 0) {
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    public static function GetAllTrajecten($begin, $startAirport, $stopAirport)
+    {
+        if (empty($startAirport) && empty($stopAirport)) {
+            $result = DbHandler::Query("SELECT * FROM traject LIMIT " . $begin . ", 5", null);
+
+        }
+        if ($startAirport && $stopAirport) {
+            $result = DbHandler::Query("SELECT * FROM traject WHERE airport_start_id = :startAirportId && airport_stop_id = :stopAirportId LIMIT " .
+                $begin . ", 5", array("startAirportId" => $startAirport, "stopAirportId" => $stopAirport));
+        }
+        if ($startAirport && empty($stopAirport)) {
+            $result = DbHandler::Query("SELECT * FROM traject WHERE airport_start_id = :startAirportId LIMIT " .
+                $begin . ", 5", array("startAirportId" => $startAirport));
+        }
+        if (empty($startAirport) && $stopAirport) {
+            $result = DbHandler::Query("SELECT * FROM traject WHERE airport_stop_id = :stopAirportId LIMIT " .
+                $begin . ", 5", array("stopAirportId" => $stopAirport));
+        }
+        for ($i = 0; $i < count($result); $i++) {
             $result[$i]['airport_start_id'] = airports::GetAirportByID($result[$i]['airport_start_id']);
             $result[$i]['airport_stop_id'] = airports::GetAirportByID($result[$i]['airport_stop_id']);
-         }
-         
-         if ($result == null) {
-             return null;
-         } else {
-			return $result;
-         }
-     }
-    
-#     public function GetAllTrajecten()
-#     {
-#         $traject = new trajecten();
-#         $result = DbHandler::Query("SELECT * FROM traject", null);
-#         if ($result == null) {
-#             return null;
-#         } else {
-#             $traject->Airport1 = airports::GetAirportByID($result[0]["airport_start_id"]);
-#             $traject->Airport2 = airports::GetAirportByID($result[0]["airport_stop_id"]);
-#             return $traject;
-#         }
-#     }
- 
+        }
+
+        if ($result == null) {
+            return null;
+        } else {
+            return $result;
+        }
+    }
 
     //Get traject by Id.
-    public function GetTraject($trajectId)
+    public static function GetTraject($trajectId)
     {
         $traject = new trajecten();
         $traject->TrajectID = $trajectId;
 
         $result = DbHandler::Query("SELECT * FROM traject WHERE traject_id = :trajectId;",
             array("trajectId" => $trajectId));
-		if (count($result) == 0) {
+        if (count($result) == 0) {
             return null;
         } else {
             $traject->Airport1 = airports::GetAirportByID($result[0]["airport_start_id"]);
@@ -89,7 +119,7 @@ class trajecten
     }
 
     //Get traject by airports.
-    public function GetTrajectByCity($startAirport, $stopAirport)
+    public static function GetTrajectByCity($startAirport, $stopAirport)
     {
         $result = DbHandler::QueryScalar("SELECT * FROM trajecten WHERE trajecten.airport_start_id = (SELECT airport_id FROM airports WHERE name = :startAirport) AND airport_stop_id = (SELECT airport_id FROM airports WHERE name = :stopAirport);",
             array("startAirport" => $startAirport, "stopAirport" => $stopAirport));
