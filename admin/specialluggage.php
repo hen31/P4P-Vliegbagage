1f36fced8168079eadc90c63bed0c0c2d9bef987
Ivar <ivardelelange2003@hotmail.com>
2013-05-27 12:13:27 +0200

    Feest YOLO SWAG Naar huis
    eten?

--- admin/specialluggage.php
+++ admin/specialluggage.php
@@ -8,9 +8,9 @@ require_once ("bovenkant.php");
     <ul>
         <li>
             <a<?php echo (isset($_GET["action"]) && $_GET["action"] == "add" ?
-                ' class="active" ' : "") ?> href="specialluggage.php?action=add">Toevoegen</a>
+' class="active" ' : "") ?> href="specialluggage.php?action=add">Toevoegen</a>
             <a<?php echo (isset($_GET["action"]) && $_GET["action"] == "edit" ?
-                ' class="active" ' : "") ?> href="specialluggage.php?action=edit">Beheren</a>
+    ' class="active" ' : "") ?> href="specialluggage.php?action=edit">Beheren</a>
         </li>
     </ul>
 </div>
@@ -20,32 +20,44 @@ require_once ("bovenkant.php");
 $specialeBagage;
 $name;
 //Toevoegen van Specialluggage:
-if (isset($_GET["action"]))
-{ 
-    if ($_GET["action"] == "add")
-    {
-        if (!empty($_POST["name"]))
-        {
-            if (strlen($_POST["name"]) > 0 && strlen ($_POST["name"]) < 51 )
-            {
+if (isset($_GET["action"])) {
+    if ($_GET["action"] == "add") {
+        if (!empty($_POST["name"])) {
+            if (strlen($_POST["name"]) > 0 && strlen($_POST["name"]) < 51) {
                 $name = $_POST["name"];
-                $CheckIfExists = specialluggage::GetSpecialLuggageName($name);
-                
-                if ($CheckIfExists != null)
-                {
-                    $name = "Dit type bagage bestaat al!"; 
-                }
-                else
-                {
+                $CheckIfExists = specialluggage::GetSpecialLuggageByName($name);
+
+                if ($CheckIfExists != null) {
+                    $name = "Dit type bagage bestaat al!";
+                } else {
                     $_POST["name"] = null;
                     $specialeBagage = specialluggage::AddItem(($name));
                 }
+            } else {
+                $name = "Niet alle velden zijn correct ingevuld!";
             }
+        } else {
+            $name = "";
         }
+?>
+        <br />
+        <table>
+        <tr>
+        <td>
+        <h1 style="margin-left: 20px;"> Speciale Bagage toevoegen</h1><br />
+        </td>
+        </tr>
+        </table>
+        <form action="specialluggage.php?action.add" method="post" class="form">
+            <div><label for="specialluggagename">Speciale bagage naam: </label><input name="name" id="specialluggagename" /></div>
+            <div>&nbsp;</div>
+            <div><label>&nbsp;</label><input type="submit" value="Speciale bagage toevoegen"/></div>
+        </form>
     }
+    
 }
 
 ?>
 <?php
-require_once ("onderkant.php");
+        require_once ("onderkant.php");
 ?>
