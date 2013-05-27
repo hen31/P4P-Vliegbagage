6b8ef27515d6f792f32cce29dd303b114b6b1639
Ivar <ivardelelange2003@hotmail.com>
2013-05-27 11:15:24 +0200

    Specialluggage
    Niels was 15 min te laat en voelt zich ziek

--- admin/specialluggage.php
+++ admin/specialluggage.php
@@ -1,13 +1,51 @@
 <?php
 //Alle data classes includen
-require_once("../data/includeAll.php");
+require_once ("../data/includeAll.php");
 $titel = "Speciale bagage";
-require_once("bovenkant.php");
+require_once ("bovenkant.php");
 ?>
-<!-- Hier alles neerzetten-->
-
-
+<div id="menu">
+    <ul>
+        <li>
+            <a<?php echo (isset($_GET["action"]) && $_GET["action"] == "add" ?
+                ' class="active" ' : "") ?> href="specialluggage.php?action=add">Toevoegen</a>
+            <a<?php echo (isset($_GET["action"]) && $_GET["action"] == "edit" ?
+                ' class="active" ' : "") ?> href="specialluggage.php?action=edit">Beheren</a>
+        </li>
+    </ul>
+</div>
+<div style="clear: both;"></div>
 
 <?php
-require_once("onderkant.php");
+$specialeBagage;
+$name;
+//Toevoegen van Specialluggage:
+if (isset($_GET["action"]))
+{ 
+    if ($_GET["action"] == "add")
+    {
+        if (!empty($_POST["name"]))
+        {
+            if (strlen($_POST["name"]) > 0 && strlen ($_POST["name"]) < 51 )
+            {
+                $name = $_POST["name"];
+                $CheckIfExists = specialluggage::GetSpecialLuggageName($name);
+                
+                if ($CheckIfExists != null)
+                {
+                    $name = "Dit type bagage bestaat al!"; 
+                }
+                else
+                {
+                    $_POST["name"] = null;
+                    $specialeBagage = specialluggage::AddItem(($name));
+                }
+            }
+        }
+    }
+}
+
+?>
+<?php
+require_once ("onderkant.php");
 ?>
