4042c9cec4fb337f41cef00abeec7abf9005498f
Ivar <ivardelelange2003@hotmail.com>
2013-05-28 10:14:36 +0200

    Fix admin und data specialluggage
    Mijn koffie was wel erg sterk, moet ik eerlijk mee zijn!!!

--- admin/specialluggage.php
+++ admin/specialluggage.php
@@ -4,6 +4,7 @@ require_once ("../data/includeAll.php");
 $titel = "Speciale bagage";
 require_once ("bovenkant.php");
 ?>
+<div>
 <div id="menu">
     <ul>
         <li>
@@ -18,10 +19,11 @@ require_once ("bovenkant.php");
 
 <?php
 $specialeBagage;
-$name;
+
 //Toevoegen van Specialluggage:
 if (isset($_GET["action"])) {
     if ($_GET["action"] == "add") {
+        $name;
         if (!empty($_POST["name"])) {
             if (strlen($_POST["name"]) > 0 && strlen($_POST["name"]) < 51) {
                 $name = $_POST["name"];
@@ -31,7 +33,7 @@ if (isset($_GET["action"])) {
                     $name = "Dit type bagage bestaat al!";
                 } else {
                     $_POST["name"] = null;
-                    $specialeBagage = specialluggage::AddItem(null,($name), null);
+                    $specialeBagage = specialluggage::AddItem(null, ($name), null);
                 }
             } else {
                 $name = "Niet alle velden zijn correct ingevuld!";
@@ -39,7 +41,8 @@ if (isset($_GET["action"])) {
         } else {
             $name = "";
         }
-    }
+
+
 ?>
         <br />
         <table>
@@ -55,29 +58,184 @@ if (isset($_GET["action"])) {
             <div><label>&nbsp;</label><input type="submit" value="Speciale bagage toevoegen"/></div>
         </form>
         <br />
+        
         <?php
 
-    if (!empty($specialeBagage)) {
+
+        if (!empty($specialeBagage)) {
 ?>
             <div style="margin-left: 20px;">
                 <br /><h1>Speciale bagage toegevoegd!</h1><br /><br /> 
-                Specialebagage id: <?php echo $specialeBagage->specialluggage_id[0]["specialluggage_id"]; ?><br /> 
+                Specialebagage id: <?php echo $specialeBagage->
+            specialluggage_id[0]["specialluggage_id"]; ?><br /> 
                 Specialbagage naam: <?php echo $specialeBagage->name; ?>
             </div>
             <?php
-    } else {
-        echo $name;
+        }
+        else
+        {
+            echo $name;
+        }
 
+    }
+    if ($_GET["action"] == "edit") {
+        if (isset($_GET["zoekQuery"])) {
+            $Specialluggage = specialluggage::SearchSpecialLuggage($_POST["Zoekveld"]);
+            $listnumber = 0;
+
+        } else {
+            $Specialluggage = specialluggage::GetSpecialLuggageList();
+            $listnumber = 0;
+        }
+        if (count($Specialluggage) < 35) {
+            if (count($Specialluggage) < 15) {
+                $listnumber = 15;
+            } else {
+                $listnumber = count($Specialluggage);
+            }
+        } else {
+            $listnumber = 35;
+        }
+?>
+        <br />
+        <table>
+            <tr>
+                <td>
+                    <h1 style="margin-left: 20px;">Speciale bagage bewerken</h1><br />
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table style="width: 880px;">
+                        <tr>
+                            <td>
+                            
+                                <div style="width:380px;">
+                                    <form action="specialluggage.php?action=edit&ItemSelected=true&zoekQuery=true" method="post" >
+                                            <div><input name="Zoekveld" id="Zoekveld" style="width:250px;" /><input type="submit" style="width:100px;" value="Zoeken"/></div>
+                                            
+                                    </form>
+                                    
+                                    <form action="speciallagguge.php?action=edit&ItemSelected=true" method="post" >
+                                        <select name="Specialluggage" multiple="true" size="<?php echo
+        $listnumber; ?>;" style="width:350px;">
+                                        <?php
+        foreach ($Specialluggage as $Specialluggage) {
+?>
+                                                <option value="<?php echo $Specialluggage->
+            Specialluggage_id; ?>"> <?php echo $Specialluggage->Name; ?></option>
+                                                <?php
+        }
+
+?>
+                                        </select>
+                                        <input type="submit" style="width:350px;" value="Geselecteerd Speciale bagage bewerken"/></div>
+                                    </form>
+                                    
+                                 </div>
+                            </td>
+                            <td style="width: 499px;" >
+                                <div style="height: <?php echo $listnumber * 15; ?>  px; width: *%;">
+<?php
+
+        if (isset($_GET["ItemSelected"])) {
+            //checken of  er een airport is gelecteerd om te wijzigen
+            if (isset($_POST["Specialluggage"])) {
+?>
+                                                            <div>
+                                                                <h1>
+                                                                <?php
+                $specialeBagage = specialluggae::GetSpecialLuggageID($_POST["Specialluggage"]);
+                echo $specialeBagage->Name;
+
+                $Name = explode("(", $specialeBagage->Name);
+
+
+                $specialluggage_id = $_POST["Specialeluggage"];
+
+                //verwijdercheckbox is scheef
+
+?>
+                                                                </h1>
+                                                                <br />
+                                                                <br /> 
+                                                                <form action="specialluggage.php?action=edit&Edited=<?php echo
+                $Airportid; ?>" method="post" class="form">
+                                                                    <div><label for="specialluggagename">Speciale bagage naam: </label><input name="name" id="specialluggagename" value="<?php echo
+                $AirportName; ?>" style="width:325px;" /></div>
+                                                                    
+                                                                    <div><label for="Verwijderen">Speciale bagage verwijderen? </label><input type="checkbox" name="verwijderen" value="true"/></div>
+                                                                    <div>&nbsp;</div>
+                                                                    <div><label>&nbsp;</label><input type="submit" value="Speciale bagage wijzigen"/></div>
+                                                                </form> 
+                                                            </div>
+                                                        <?php
+            } else {
+                if (isset($_GET["zoekQuery"])) {
+                    echo "<h1>Er is gezocht op: " . $_POST["Zoekveld"] . "</h1><br />";
+                    echo "Stappen om een vliegveld te bewerken of te verwijderen: <br /><br />";
+                    echo "Stap 1: Selecteer een vliegveld in de linkerlijst. <br />Stap 2: Klik op bewerken om een vliegveld te bewerken.";
+                } else {
+                    echo "<h1>Fout!</h1><br />";
+                    echo "Voor dat er op bewerken wordt geklikt moet er een item aan de linkerzijde worden geselecteerd";
+                }
+            }
+        } else {
+            //item moet worden aangepast in de database
+            if (isset($_GET["Edited"])) {
+                if (strlen($_POST["name"]) > 0 && strlen($_POST["name"]) < 101) {
+                    $Verwijderen = "";
+                    $name = $_POST["name"];
+                    if (isset($_POST["verwijderen"])) {
+                        $Verwijderen = $_POST["verwijderen"];
+                    }
+                    $ItemID = $_GET["Edited"];
+
+                    if ($Verwijderen == "true") {
+                        airports::RemoveItem($ItemID);
+                    } else {
+                        airports::EditItem($ItemID, $FullName);
+                    }
+
+                    echo "
+                                                        <h1>
+                                                        Bewerken succesvol.
+                                                        </h1> 
+                                                        <script>
+                                                        windows.location = admin/airports.php?action=edit
+                                                        </script>
+                                                        <br />
+                                                        ";
+                } else {
+                    echo "
+                                                        <h1>
+                                                        Bewerken niet succesvol. <br /><br />
+                                                        </h1>
+                                                        
+                                                        Niet alle velden zijn juist ingevuld. <br /><br />
+                                                        Denk om het volgende:<br />
+                                                        
+                                                        *Vliegveld naam mag niet langer zijn dan 100 tekens.<br />
+                                                        *IATA code mag niet langer zijn dan 4 tekens.<br /><hr /><br />
+                                                        ";
+                }
+
+
+            }
+            echo "Stappen om een vliegveld te bewerken of te verwijderen: <br /><br />";
+            echo "Stap 1: Selecteer een vliegveld in de linkerlijst. <br />Stap 2: Klik op bewerken om een vliegveld te bewerken.";
+        }
     }
 }
 ?>
-
-<?php
-if (isset($_GET["action"])) {
-    if ($_GET["action"] == "edit") {
-        }
-        }
-?>
+                                </div>
+                            </td>
+                        </tr>    
+                    </table>
+                
+                </td>
+            </tr>        
+        </table>  
 
 <?php
 require_once ("onderkant.php");
--- data/specialluggage.php
+++ data/specialluggage.php
@@ -1,8 +1,8 @@
 <?php
 /**
-* @Auteur Ivar de Lange & Teake Otter
-* @Datum 21-5-2013
-*/
+ * @Auteur Ivar de Lange & Teake Otter
+ * @Datum 21-5-2013
+ */
 
 require ("includeAll.php");
 
@@ -12,7 +12,7 @@ class SpecialLuggage
     public $AirlineID;
     public $Name;
     public $Notes;
-    
+
     public function SetProperties($specialluggage_id, $AirlineID, $Name, $Notes)
     {
         $this->specialluggage_id = $specialluggage_id;
@@ -20,103 +20,141 @@ class SpecialLuggage
         $this->Name = $Name;
         $this->Notes = $Notes;
     }
-    
+     public function SetPropertiestwo($specialluggage_id, $name)
+    {
+        $this->specialluggage_id = $specialluggage_id;
+        $this->Name = $name;
+    }
+
     public static function AddItem($AirlineID, $Name, $Notes)
-    {  
-        $QueryResult = DbHandler::Query("SELECT specialluggage_id FROM specialluggage WHERE name = :Name ;", array("Name" => $Name));
-        
-        if ($QueryResult != null && count($QueryResult) > 0)
-        {
+    {
+        $QueryResult = DbHandler::Query("SELECT specialluggage_id FROM specialluggage WHERE name = :Name ;",
+            array("Name" => $Name));
+
+        if ($QueryResult != null && count($QueryResult) > 0) {
             $SpecialLugageID = $QueryResult[0]["specialluggage_id"];
-        }
-        else
-        {
-            DbHandler::NonQuery("INSERT INTO specialluggage (name) VALUES (:Name); ", array("Name" => $Name));
-            $Result = DbHandler::Query("SELECT specialluggage_id FROM specialluggage WHERE name = :Name ;", array("Name" => $Name));
+        } else {
+            DbHandler::NonQuery("INSERT INTO specialluggage (name) VALUES (:Name); ", array
+                ("Name" => $Name));
+            $Result = DbHandler::Query("SELECT specialluggage_id FROM specialluggage WHERE name = :Name ;",
+                array("Name" => $Name));
             $SpecialLugageID = $Result[0]["specialluggage_id"];
         }
-        if($AirlineID !=null)
-        {
-        
-        DbHandler::NonQuery("INSERT INTO airlinespecialluggage (airline_id, specialluggage_id, notes) VALUES (:Airlineid, :specialid, :notes); ", array("Airlineid" => $AirlineID, "specialid" => $SpecialLugageID, "notes" => $Notes));
-        $ClassObject = new SpecialLuggage();
-        $ClassObject->SetProperties($SpecialLugageID, $AirlineID, $Name, $Notes);
-        return $ClassObject;
+        if ($AirlineID != null) {
+
+            DbHandler::NonQuery("INSERT INTO airlinespecialluggage (airline_id, specialluggage_id, notes) VALUES (:Airlineid, :specialid, :notes); ",
+                array(
+                "Airlineid" => $AirlineID,
+                "specialid" => $SpecialLugageID,
+                "notes" => $Notes));
+            $ClassObject = new SpecialLuggage();
+            $ClassObject->SetProperties($SpecialLugageID, $AirlineID, $Name, $Notes);
+            return $ClassObject;
         }
-        
+
     }
     public static function EditAirlineNotes($Specialluggage_id, $airlineID, $Notes)
     {
-        if ($airlineID != 0 && $notes != "")
-        {
-            DbHandler::NonQuery("UPDATE airlinespecialluggage SET airline_id = :airlineID, notes = :notes WHERE specialluggage_id = :ID", array("airline_id" => $airlineID, "ID" => $Specialluggage_id, "notes" => $Notes));
+        if ($airlineID != 0 && $notes != "") {
+            DbHandler::NonQuery("UPDATE airlinespecialluggage SET airline_id = :airlineID, notes = :notes WHERE specialluggage_id = :ID",
+                array(
+                "airline_id" => $airlineID,
+                "ID" => $Specialluggage_id,
+                "notes" => $Notes));
             return SpecialLuggage::GetCombo($airlineID, $Specialluggage_id);
         }
     }
-    
+
     public static function EditAirlineName($Specialluggage_id, $Name)
     {
-        
-            DbHandler::NonQuery("UPDATE specialluggage SET name = :Name WHERE specialluggage_id = :ID", array("Name" => $Name, "ID" => $Specialluggage_id));
-            return SpecialLuggage::GetCombo($airlineID, $Specialluggage_id);
+
+        DbHandler::NonQuery("UPDATE specialluggage SET name = :Name WHERE specialluggage_id = :ID",
+            array("Name" => $Name, "ID" => $Specialluggage_id));
+        return SpecialLuggage::GetCombo($airlineID, $Specialluggage_id);
     }
-    
+
     public static function RemoveSpecialLuggage($Specialluggage_id)
     {
-        DbHandler::NonQuery("DELETE FROM specialluggage WHERE specialluggage_id = :ID", array("ID" => $Specialluggage_id));
-        DbHandler::NonQuery("DELETE FROM airlinespecialluggage WHERE specialluggage_id = :ID", array("ID" => $Specialluggage_id));
+        DbHandler::NonQuery("DELETE FROM specialluggage WHERE specialluggage_id = :ID",
+            array("ID" => $Specialluggage_id));
+        DbHandler::NonQuery("DELETE FROM airlinespecialluggage WHERE specialluggage_id = :ID",
+            array("ID" => $Specialluggage_id));
     }
-    
+
     public static function RemoveAirLineSpecialLuggage($SpecialLuggage_id, $AirlineID)
     {
-        DbHandler::NonQuery("DELETE FROM airlinespeciallluggage WHERE speciallugage_id = :SID AND airline_id = :AID ;", array("SID" => $SpecialLuggage_id, "AID" => $AirlineID));
-        $query = DbHandler::Query("SELECT Count(Specialluggage_id) AS csl FROM airlinespecialluggage WHERE specialluggage_id = :SID ;", array("SID" => $SpecialLuggage_id));
-        
-        if ($query[0]["csl"] == 0)
-        {
-             DbHandler::NonQuery("DELETE FROM specialluggage WHERE specialluggage_id = :ID", array("ID" => $Specialluggage_id));
-        }        
-    }    
+        DbHandler::NonQuery("DELETE FROM airlinespeciallluggage WHERE speciallugage_id = :SID AND airline_id = :AID ;",
+            array("SID" => $SpecialLuggage_id, "AID" => $AirlineID));
+        $query = DbHandler::Query("SELECT Count(Specialluggage_id) AS csl FROM airlinespecialluggage WHERE specialluggage_id = :SID ;",
+            array("SID" => $SpecialLuggage_id));
+
+        if ($query[0]["csl"] == 0) {
+            DbHandler::NonQuery("DELETE FROM specialluggage WHERE specialluggage_id = :ID",
+                array("ID" => $Specialluggage_id));
+        }
+    }
     public static function GetSpecialLuggageID($ID)
     {
-        $Result = DbHandler::Query("SELECT * FROM specialluggage WHERE specialluggage_id = :ID ;", array("ID" => $ID));   
+        $Result = DbHandler::Query("SELECT * FROM specialluggage WHERE specialluggage_id = :ID ;",
+            array("ID" => $ID));
         $ClassObject = new SpecialLuggage();
-        $ClassObject->SetProperties($ID, 0,$Result[0]["name"],"");
+        $ClassObject->SetProperties($ID, 0, $Result[0]["name"], "");
         return $ClassObject;
     }
-    
+
     public static function GetSpecialLuggageName($Name)
     {
-        $Result = DbHandler::Query("Select * FROM specialluggage WHERE  NAME = :Name;", array("Name" => $Name));
-        if(count($Result) < 1){
+        $Result = DbHandler::Query("Select * FROM specialluggage WHERE  NAME = :Name;",
+            array("Name" => $Name));
+        if (count($Result) < 1) {
             return null;
         }
         $ClassObject = new SpecialLuggage();
-        $ClassObject->SetProperties($Result[0]["specialluggage_id"], 0,$Result[0]["name"],"");
+        $ClassObject->SetProperties($Result[0]["specialluggage_id"], 0, $Result[0]["name"],
+            "");
         return $ClassObject;
     }
-      
+
     public static function GetCombo($airlineID, $specialLuggageID)
     {
-        $Result = DbHandler::Query("SELECT * FROM specialluggage WHERE specialluggage_id = :ID ;", array("ID" => $specialLuggageID));
-        $Result2 = DbHandler::Query("SELECT * FROM airlinespecialluggage WHERE airline_id = :AirlineID ;", array("AirlineID" => $airlineID));   
+        $Result = DbHandler::Query("SELECT * FROM specialluggage WHERE specialluggage_id = :ID ;",
+            array("ID" => $specialLuggageID));
+        $Result2 = DbHandler::Query("SELECT * FROM airlinespecialluggage WHERE airline_id = :AirlineID ;",
+            array("AirlineID" => $airlineID));
         $ClassObject = new SpecialLuggage();
-        $ClassObject->SetProperties($specialLuggageID, $airlineID,$Result[0]["name"], $Result2[0]["notes"]);
+        $ClassObject->SetProperties($specialLuggageID, $airlineID, $Result[0]["name"], $Result2[0]["notes"]);
         return $ClassObject;
     }
-    
+
     public static function GetSpecialLuggageList()
     {
         $Query = DbHandler::Query("SELECT * FROM SPECIALLUGGAGE", null);
         $SpecialLuggageCollection = array();
-        
-        foreach($Query as $result)
-               {
-                    $SpecialLuggageObject = new SpecialLuggage();
-                    $SpecialLuggageObject -> SetProperties($result["specialluggage_id"], 0, $result["name"], "");
-                    array_push($SpecialLuggageCollection, $SpecialLuggageObject);
-               }
+
+        foreach ($Query as $result) {
+            $SpecialLuggageObject = new SpecialLuggage();
+            $SpecialLuggageObject->SetProperties($result["specialluggage_id"], 0, $result["name"],
+                "");
+            array_push($SpecialLuggageCollection, $SpecialLuggageObject);
+        }
         return $SpecialLuggageCollection;
+    }
+    
+    public static function SearchSpecialLuggage($SearchQuery)
+    {
+        $Query = DbHandler::Query("SELECT * FROM specialluggage WHERE name LIKE  :SearchQuery ",
+            array("SearchQuery" => "%" . $SearchQuery . "%"));
+        $SpecialLuggageCollection = array();
+
+        foreach ($Query as $result) {
+            $SpecialLuggageObject = new SpecialLuggage();
+            $SpecialLuggageObject->SetPropertiestwo($result["specialluggage_id"], $result["name"]);
+            array_push($SpecialLuggageCollection, $SpecialLuggageObject);
+        }
+
+
+        return $SpecialLuggageCollection;
+
     }
 }
 
