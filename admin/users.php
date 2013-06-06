8c70b5d91181906220671684b5354ee064b3f150
Matthe Jacobs <m_jacobs@hotmail.nl>
2013-06-06 09:11:54 +0200

    Code der codes

--- admin/users.php
+++ admin/users.php
@@ -1,6 +1,8 @@
 <?php
 //Alle data classes includen
 require_once ("../data/includeAll.php");
+$text1 = ' ';
+$text2 = ' ';
 
 $fatalerror = false;
 $submitUser = null;
@@ -9,28 +11,67 @@ $submitUser = null;
 $titel = "Gebruikers";
 require_once ("bovenkant.php");
 
-if(isset($_GET['id']))
-{
-    if(isset($_GET['actie'])&& $_GET['actie'] == 'Delete')
-    {
-        user::deleteUser($_GET['id']);
+if (isset($_POST['editUsername'])) {
+    //Check of oude gebruikdersnaam en oude wachtwoord overeen komen.
+    //Check of de textboxes niet leeg zijn
+    if (!empty($_SESSION['user_id'])) {
+        if ( !empty($_POST['newUser']) ) {
+            if (!user::UsernameExists($_POST['newUser'])) {
+                    $changeCheck = user::changeUser($_SESSION['user_id'], $_POST['newUser'],null);
+                    unset($_SESSION['user_id']);
+                    $text1 = 'Gebruikersnaam gewijzigd';    
+            } else {
+                    $text1 = 'Nieuwe gebruikersnaam bestaat al';
+                }
+        } else {
+            $text1 = '1 of meer velden zijn niet ingevuld';
+        }
     }
-  
-    $submitUser = user::GetUser($_GET['id']);
+}
+
+if (isset($_POST['editPassword'])) {
+    if (!empty($_SESSION['user_id'])) {
+        if (!empty($_POST['oldPass']) && !empty($_POST['newPass'])) {
+                $changeCheck = user::changeUser($_SESSION['user_id'], null, $_POST['newPass']);
+                unset($_SESSION['user_id']);
+                if ($changeCheck) {
+                    $text1 = 'Wachtwoord gewijzigd';
+                } else {
+                    $text1 = 'Wachtwoord wijzigen mislukt';
+                }
+            } 
+        } else {
+            $text1 = '1 of meer velden zijn leeg';
+        }
+    }
+
+if (isset($_GET['action']) && isset($_GET['actie'])&&$_GET['actie'] == 'Delete') {
+        $check = user::deleteUser($_GET['id']);
+        if($check){
+            $text1 = 'Gebruiker verwijderd';
+        }
+        else{
+            $text1 = 'Gebruiker kan niet verwijderd worden. Moet minstens 1 gebruiker blijven bestaan';
+        }
+}
+
+if (isset($_GET['id'])) {
     
+
+    $submitUser = user::GetUser($_GET['id']);
+
     //Check of submit user niet leeg is
-    if($submitUser != null)
-    {
-    $_SESSION["user_id"] =  $_GET['id'];
+    if ($submitUser != null) {
+        $_SESSION["user_id"] = $_GET['id'];
     }
- 
 }
 ?>
+
 <div id="menu">
     <ul>
         <li>
             <a<?php echo (isset($_GET["action"]) && $_GET["action"] == "add" ?
-    ' class="active" ' : "") ?> href="users.php?action=add">Toevoegen</a>
+' class="active" ' : "") ?> href="users.php?action=add">Toevoegen</a>
         </li>
         <li>
             <a<?php echo (isset($_GET["action"]) && $_GET["action"] == "edit" ?
@@ -40,28 +81,31 @@ if(isset($_GET['id']))
 </div>
 <div style="clear: both;"></div>
 
+<br />
+<h1>Gebruikers</h1><br />
+<p>Gebruik het menu om een gebruiker toe te voegen of te bewerken.</p>
+
 <?php
 //Gebruiker toevoegen veld
-if (isset($_GET["action"])) 
-{
-    if ($_GET["action"] == "add") 
-    {
-        if (isset($_POST['addUser']))
-         {
-            if($_POST['password'] == $_POST['passControle'])
-            {
-                if (!empty($_POST['username']) && !empty($_POST['password'])) {                    
-                    $user = user::createUser($_POST["username"], $_POST["password"]);
+if (isset($_GET["action"])) {
+    if ($_GET["action"] == "add") {
+        if (isset($_POST['addUser'])) {
+            if (!user::UsernameExists($_POST['username'])) {
+                if ($_POST['password'] == $_POST['passControle']) {
+                    if (!empty($_POST['username']) && !empty($_POST['password'])) {
+                        $addedCheck;
+                        $addedCheck = $user = user::createUser($_POST["username"], $_POST["password"]);
+                    } else {
+
+                    }
                 } else {
-                    
+                    $text2 = 'Wachtwoorden komen niet overeen!';
                 }
-            }
-            else
-            {
-                echo "Wachtwoorden komen niet overeen!";
+            } else {
+                $text2 = 'Gebruikersnaam bestaat al';
             }
         }
-    
+
 ?>
 
 <br />
@@ -70,10 +114,11 @@ if (isset($_GET["action"]))
             <tr>
                 <td>
                     <h1>Gebruiker toevoegen</h1><br />
-                </td>
+                </td>                
             </tr>
             </table>
             <br />
+            <?php echo $text2; ?>
             <form action="users.php?action=add" method="post" class="form">
                 <div><label for="Gebruikersnaam">Gebruikersnaam: </label><input name="username" id="username" /></div>
                 <div><label for="Wachtwoord">Wachtwoord: </label><input name="password" id="password" type="password"></div>
@@ -90,58 +135,27 @@ if (isset($_GET["action"]))
         $users = user::GetUsers('');
 
         echo "<table>";
-        if ($users != null) 
-        {
-            foreach ($users as $user) 
-            {
+        if ($users != null) {
+            foreach ($users as $user) {
                 //Table waar gebruikers in worden getoond.
-                echo '<tr><td>' . $user->userName .
-                    '</td><td><a href="users.php?actie=Change&id=' . $user->id .
-                    '&action=edit">Bewerken</a></td><td><a href="users.php?actie=Delete&id=' . $user->id .
-                    '&action=edit">Verwijderen</a></td></tr>';
+                echo '<tr><td>' . $user->userName . '</td>
+                    <td><a href="users.php?action=editUser&id=' . $user->id .
+                    '&action=editUser">Gebruikersnaam wijzigen</a></td>
+                    <td><a href="users.php?action=editPass&id=' . $user->id .
+                    '&action=editPass">Wachtwoord wijzigen</a></td>
+                    <td><a href="users.php?action=edit&id=' . $user->id .
+                    '&actie=Delete">Verwijderen</a></td>
+                    </tr>';
             }
         }
         echo "</table>";
-        
-        //Stuk code voor veranderen van de gebruiker
-        if (isset($_POST['changeUser'])) 
-        {
-            //Check of oude gebruikdersnaam en oude wachtwoord overeen komen.
-            if (user::checkUser($_POST["oldUser"], user::incryptPass($_POST["oldPass"]))) 
-            {
-                if(!empty($_SESSION["user_id"]))
-                {
-                //Check of de textboxes niet leeg zijn
-                    if(!empty($_POST['newUser']) && !empty($_POST['newPass']))
-                    {
-                    $changeCheck = user::changeUser($_SESSION["user_id"] , ($_POST["newUser"]), $_POST["newPass"]);
-                    unset($_SESSION["user_id"]);
-                        if($changeCheck)
-                        {
-                            echo 'Gebruiker gewijzigd';
-                        }
-                        else
-                        {
-                            echo 'Gebruikersnaam bestaat al';
-                        }    
-                
-                    }
-                    else
-                    {
-                    //De tekst 'Er is een leeg veld' wordt geprint
-                        echo 'Er is een leeg veld';
-                    }
-                }
-                else
-                {
-                    echo 'Druk op bewerken bij een gebruiker';
-                }
-            }
-            else
-            {
-                echo "Niet alles is ingevuld";
-            }
-        }
+?><br />
+        <?php
+        echo $text1;
+    }
+
+    //Stuk code voor veranderen van de gebruiker
+    if ($_GET['action'] == 'editUser') {
 ?>
 
 <br />
@@ -149,30 +163,49 @@ if (isset($_GET["action"]))
     <table>
         <tr>
             <td>
-                <h1>Gebruiker wijzigen</h1><br />
+                <h1><?php echo isset($submitUser) ? $submitUser->userName : '' ?> wijzigen</h1><br />
             </td>
         </tr>
     </table>
 <br />
+<?php echo $text1; ?>
         <form action="users.php?action=edit" method="post" class="form">
-            <div><label for="Oude gebruikersnaam">Oude gebruikersnaam: </label><input name="oldUser" id="oldUser" 
-            value="<?php echo isset($submitUser)  ?  $submitUser->userName : ''?>" /></div>
-            <div><label for="Oud wachtwoord">Oud wachtwoord: </label><input name="oldPass" id="oldPass" type="password"></div>
             <div><label for="Nieuwe gebruikersnaam">Nieuwe gebruikersnaam: </label><input name="newUser" id="newUser" /></div>
-            <div><label for="Nieuw Wachtwoord">Nieuw wachtwoord: </label><input name="newPass" id="newPass" type="password"></div>
             <div>&nbsp;</div>
-            <div><label>&nbsp;</label><input type="submit" name="changeUser" value="Verander gebruiker"/></div>
-        </form>            
+            <div><label>&nbsp;</label><input type="submit" name="editUsername" value="Gebruikersnaam wijzigen"/></div>
+        </form>
 </div>
 <br />
 
 
 <?php
-}
-}
-if ($fatalerror) 
-{
-echo "Niet alle velden zijn ingevuld.";
+    }
+
+    if ($_GET['action'] == 'editPass') {
+
+
+
+?>
+<br />
+<div>
+    <table>
+        <tr>
+            <td>
+                <h1><?php echo isset($submitUser) ? $submitUser->userName : '' ?>'s wachtwoord wijzigen</h1><br />
+            </td>
+        </tr>
+    </table>
+<br />
+        <form action="users.php?action=edit" method="post" class="form">
+          
+            <div><label for="Oud wachtwoord">Oud wachtwoord: </label><input name="oldPass" id="oldPass" type="password"></div>
+            <div><label for="Nieuw wachtwoord">Nieuw wachtwoord: </label><input name="newPass" id="newPass" type="password"></div>
+            <div>&nbsp;</div>
+            <div><label>&nbsp;</label><input type="submit" name="editPassword" value="Wachtwoord wijzigen"/></div>
+        </form>
+</div>  
+<?php
+    }
 }
 require_once ("onderkant.php");
 ?>
--- data/users.php
+++ data/users.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once("includeAll.php");
+require_once ("includeAll.php");
 /**
- * @Auteur Mathé Jacobs
+ * @Auteur Matthé Jacobs
  * @Datum 13-5-2013
  * @uses in deze class zitten alle database code voor het maken, wijzigen en verwijderen.
  */
@@ -11,160 +11,173 @@ class user
     public $id;
     public $userName;
     private static $SALT = "iefoafinfawhq91q9h''''aoiash[][]{}}{";
-    
-    //Set properties    
+
+    //Set properties
     public function SetProperties($id, $Name)
     {
-            $this->userid = $id;
-            $this->user = $Name; 
+        $this->userid = $id;
+        $this->user = $Name;
     }
-    
+
     //Encrypt wachtwoord functie.
     public static function incryptPass($pass)
     {
         $incryptPass = sha1($pass . user::$SALT);
-        
+
         return $incryptPass;
     }
-            
-    //Een check die kijkt of de username al bestaat. Geeft true of false terug.    
-    private static function UsernameExists($username)
+
+    //Een check die kijkt of de username al bestaat. Geeft true of false terug.
+    public static function UsernameExists($username)
     {
-        $userExist = DbHandler::Query("SELECT username FROM user WHERE username = :username", array("username"=> $username));
-        
+        $userExist = DbHandler::Query("SELECT username FROM user WHERE username = :username",
+            array("username" => $username));
+
         //Check of gebruiker wel bestaat
-        if($userExist !=null && $userExist[0]["username"] == $username){
+        if ($userExist != null && $userExist[0]["username"] == $username) {
             return true;
-        }
-        else
-        {
-        return false; 
+        } else {
+            return false;
         }
     }
-    
+
     //Kan een nieuwe gebruiker aanmaken met wachtwoord.
-    public static function createUser($username,$userPassword)
-    {   
+    public static function createUser($username, $userPassword)
+    {
         $userPassword = sha1($userPassword . user::$SALT);
         $check = user::UserNameExists($username);
-        if($check != true)
-        {
-            DbHandler::Query("INSERT INTO user(username,password) VALUES (:Name,:userPassword);", 
-            array("Name" => $username, "userPassword" => $userPassword));
+        if ($check != true) {
+            DbHandler::Query("INSERT INTO user(username,password) VALUES (:Name,:userPassword);",
+                array("Name" => $username, "userPassword" => $userPassword));
             echo "user aangemaakt";
-        }
-        else
-        {
+
+            return true;
+        } else {
             return false;
-        }                                
+        }
     }
-    
+
     //Een gebruiker verwijderen.
     public static function deleteUser($userid)
-    {        
-            DbHandler::NonQuery("DELETE FROM user WHERE user_id =:ID;", array("ID" => $userid));
-    }
-    
-    //Gebruikersnaam en/of wachtwoord veranderen.
-    public static function changeUser($userid,$username,$userPassword)
     {
-        $check = user::UsernameExists($username);
-        $user =user::GetUser($userid);
-        $userPassword = sha1($userPassword . user::$SALT);
-        if($check == false && $user->userName == $username )
+        $countId = DbHandler::QueryScalar("SELECT COUNT(user_id) FROM user");
+        if ($countId > 1) {
+            DbHandler::NonQuery("DELETE FROM user WHERE user_id =:ID;", array("ID" => $userid));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Gebruikersnaam en/of wachtwoord veranderen.
+    public static function changeUser($userid, $username, $userPassword)
+    {
+        if($username!= null && $userPassword!= null)
         {
-           DbHandler::NonQuery("UPDATE user SET username=:Name, password = :password WHERE user_id = :ID;", 
-           array("Name" => $username, "ID" => $userid, "password" => $userPassword));
+        $check = user::UsernameExists($username);
+        $userPassword = sha1($userPassword . user::$SALT);
+        DbHandler::NonQuery("UPDATE user SET username=:Name, password = :password WHERE user_id = :ID;",
+            array(
+            "Name" => $username,
+            "ID" => $userid,
+            "password" => $userPassword));
+
+        return true;
+        }
+        else if($username!= null && $userPassword== null)
+        {
+                $check = user::UsernameExists($username);
+        $userPassword = sha1($userPassword . user::$SALT);
+        DbHandler::NonQuery("UPDATE user SET username=:Name WHERE user_id = :ID;",
+            array(
+            "Name" => $username,
+            "ID" => $userid));
+
+        return true; 
         }
         else
         {
-            return false;
-        }                                      
+        $userPassword = sha1($userPassword . user::$SALT);
+        DbHandler::NonQuery("UPDATE user SET  password = :password WHERE user_id = :ID;",
+            array(
+            "ID" => $userid,
+            "password" => $userPassword));
+
+        return true;
+        }
     }
-    
+
     //Gebruiker ophalen aan de hand van de id die wordt opgegeven
     public static function GetUser($userid)
     {
-        $results = DbHandler::Query("SELECT * FROM user WHERE user_id=:id;", array("id"=>$userid));
-        if($results !=null)
-        {
+        $results = DbHandler::Query("SELECT * FROM user WHERE user_id=:id;", array("id" =>
+                $userid));
+        if ($results != null) {
             $gebruiker = new user();
-            
-          $gebruiker->id  = $results[0]["user_id"];
-           $gebruiker->userName  = $results[0]["username"];
-           return $gebruiker;
-        }
-        else
-        {
+
+            $gebruiker->id = $results[0]["user_id"];
+            $gebruiker->userName = $results[0]["username"];
+            return $gebruiker;
+        } else {
             return null;
-        }                               
+        }
     }
-    
+
     //Alle gebruikers die in de database staan terug geven afhankelijk van de zoekterm en de hoeveelste gebruikers
     public static function GetUsers($searchTerm)
     {
-            $results = DbHandler::Query("SELECT * FROM user WHERE username LIKE :search ;", array(
-            "search"=> '%'.$searchTerm.'%'));
-            //als gebruikers leeg is null terug geven
-        if($results !=null)
-        {
-      $gebruikers = array();
-            for($i =0;$i<count($results);$i++)
-            {
-            $gebruiker = new user();
-            $gebruiker->id  = $results[$i]["user_id"];
-            $gebruiker->userName  = $results[$i]["username"];
-            //Gebruiker toevoegen aan array
-            $gebruikers[] = $gebruiker;
-           }
-           return $gebruikers;
-        }
-        else
-        {
+        $results = DbHandler::Query("SELECT * FROM user WHERE username LIKE :search ;",
+            array("search" => '%' . $searchTerm . '%'));
+        //als gebruikers leeg is null terug geven
+        if ($results != null) {
+            $gebruikers = array();
+            for ($i = 0; $i < count($results); $i++) {
+                $gebruiker = new user();
+                $gebruiker->id = $results[$i]["user_id"];
+                $gebruiker->userName = $results[$i]["username"];
+                //Gebruiker toevoegen aan array
+                $gebruikers[] = $gebruiker;
+            }
+            return $gebruikers;
+        } else {
             return null;
         }
     }
-              
-    
-    //fucntie om te checken of het een geldige login is. 
+
+
+    //fucntie om te checken of het een geldige login is.
     //als dit het geval is dan wordt er een object van gebruiker terug gegeven.
-    public static function login($username,$userPassword)
+    public static function login($username, $userPassword)
     {
-      $results = DbHandler::Query("SELECT user_id, username FROM user WHERE username=:user AND password=:pass;", array(
-            "user"=> $username, "pass"=>$userPassword));
-            //als er geen gebruiker bestaat met deze combinatie wordt null terug gegeven
-        if($results !=null)
-        {
-            
+        $results = DbHandler::Query("SELECT user_id, username FROM user WHERE username=:user AND password=:pass;",
+            array("user" => $username, "pass" => $userPassword));
+        //als er geen gebruiker bestaat met deze combinatie wordt null terug gegeven
+        if ($results != null) {
+
             $gebruiker = new user();
-            
-          $gebruiker->id  = $results[0]["user_id"];
-           $gebruiker->userName  = $results[0]["username"];
-           return $gebruiker;
-        }
-        else
-        {
-         //null terug geven   
+
+            $gebruiker->id = $results[0]["user_id"];
+            $gebruiker->userName = $results[0]["username"];
+            return $gebruiker;
+        } else {
+            //null terug geven
             return null;
         }
     }
-    
+
     //Check of gebruikersnaam en wachtwoord kloppen
     public static function checkUser($username, $userPassword)
     {
         $check;
-        $results = DbHandler::Query("SELECT * FROM user WHERE username=:user AND password=:pass;", array(
-            "user"=> $username, "pass"=>$userPassword));
-            
-        if($results != null)
-        {
+        $results = DbHandler::Query("SELECT * FROM user WHERE username=:user AND password=:pass;",
+            array("user" => $username, "pass" => $userPassword));
+
+        if ($results != null) {
             $check = true;
+        } else {
+            $check = false;
         }
-        else
-        {
-            $check = false;    
-        }
-        
+
         return $check;
     }
 }
