e118eeb89a7cb97a5f9222ce984364152c5c5217
Hendrik de Jonge <hendrikdejonge@hotmail.com>
2013-06-13 10:06:08 +0200

    1551351651651651651651651651651651651651651651651

--- data/FrontEnd.php
+++ data/FrontEnd.php
@@ -11,22 +11,14 @@ class FrontEnd
     static public function GetAirportsEndByStart($begin)
     {
          $ids = DbHandler::Query("SELECT DISTINCT(airport_id) FROM airports WHERE ( airport_id IN ( SELECT airport_stop_id FROM traject WHERE airport_start_id=:id  GROUP BY airport_stop_id)) ORDER BY airports.name ASC ;", array("id"=> $begin));
-        //$numbers =  array();
+
         $airports = array();
+        
         for ($i = 0; $i < count($ids); $i++)
         {
             $airports[] = airports::GetAirportByID($ids[$i]["airport_id"]);
-            //   $numbers[] = $ids[$i]["airport_start_id"];
-            // $numbers[] = $ids[$i]["airport_stop_id"];
         }
 
-        /* $numbers =     array_unique($numbers);
-        $airports =  array();   
-        for($c = 0;$c< count($numbers);$c++)
-        {
-        $airports[] = airports::GetAirportByID($numbers[$c]);
-        
-        }*/
         return $airports;
     }
     
@@ -168,65 +160,44 @@ class FrontEnd
        static public function GetAirportsEnd()
        {
          $ids = DbHandler::Query("SELECT DISTINCT(airport_id) FROM airports WHERE ( airport_id IN ( SELECT airport_stop_id FROM traject GROUP BY airport_stop_id)) ORDER BY airports.name ASC ;");
-        //$numbers =  array();
+
         $airports = array();
         for ($i = 0; $i < count($ids); $i++)
         {
             $airports[] = airports::GetAirportByID($ids[$i]["airport_id"]);
-            //   $numbers[] = $ids[$i]["airport_start_id"];
-            // $numbers[] = $ids[$i]["airport_stop_id"];
+
         }
 
-        /* $numbers =     array_unique($numbers);
-        $airports =  array();   
-        for($c = 0;$c< count($numbers);$c++)
-        {
-        $airports[] = airports::GetAirportByID($numbers[$c]);
-        
-        }*/
+
         return $airports;
        }
         static public function GetAirportsBegin()
        {
        $ids = DbHandler::Query("SELECT DISTINCT(airport_id) FROM airports WHERE (airport_id IN ( SELECT airport_start_id FROM traject GROUP BY airport_start_id)) ORDER BY airports.name ASC;");
-        //$numbers =  array();
+
         $airports = array();
         for ($i = 0; $i < count($ids); $i++)
         {
             $airports[] = airports::GetAirportByID($ids[$i]["airport_id"]);
-            //   $numbers[] = $ids[$i]["airport_start_id"];
-            // $numbers[] = $ids[$i]["airport_stop_id"];
+
         }
 
-        /* $numbers =     array_unique($numbers);
-        $airports =  array();   
-        for($c = 0;$c< count($numbers);$c++)
-        {
-        $airports[] = airports::GetAirportByID($numbers[$c]);
-        
-        }*/
+
         return $airports;
        }
     //Alle gebruikte vliegvelden ophalen
     static public function GetAirports()
     {
         $ids = DbHandler::Query("SELECT DISTINCT(airport_id) FROM airports WHERE (airport_id IN ( SELECT airport_start_id FROM traject GROUP BY airport_start_id) OR airport_id IN ( SELECT airport_stop_id FROM traject GROUP BY airport_stop_id)) ;");
-        //$numbers =  array();
+
         $airports = array();
         for ($i = 0; $i < count($ids); $i++)
         {
             $airports[] = airports::GetAirportByID($ids[$i]["airport_id"]);
-            //   $numbers[] = $ids[$i]["airport_start_id"];
-            // $numbers[] = $ids[$i]["airport_stop_id"];
+
         }
 
-        /* $numbers =     array_unique($numbers);
-        $airports =  array();   
-        for($c = 0;$c< count($numbers);$c++)
-        {
-        $airports[] = airports::GetAirportByID($numbers[$c]);
-        
-        }*/
+
         return $airports;
     }
 
